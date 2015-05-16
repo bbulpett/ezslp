275997a09a0142dff6053f1450d6e42439aba845
Mark Locklear <marklocklear@gmail.com>
2015-05-16 09:05:55 -0400

    updated finder to where clause

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,6 +3,8 @@ class Organization < ActiveRecord::Base
   has_many :users
   has_many :patients, :through => :users
 
+  # attr_accessible :name, :contact, :phone
+
   validates_uniqueness_of :name
   validates_presence_of :name
 end
